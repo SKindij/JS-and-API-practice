44526d20dd3e80e57f116e28d82f05f6837f5607
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-04-01 21:55:34 +0300

    Update index.js

--- marvel/src/index.js
+++ marvel/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './components/app/App';
+import MarvelService from './services/MarvelService';
 
+import './style/style.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
