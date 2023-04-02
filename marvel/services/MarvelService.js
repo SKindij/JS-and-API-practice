63ce52a707eefa42153aad4886675402ccf87327
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-04-02 13:59:01 +0300

    Create MarvelService.js

---
+++ marvel/services/MarvelService.js
@@ -0,0 +1,34 @@
+class MarvelService {
+
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  // public key
+  _apiKey = 'apikey=7021a063d0296193d0fe45e71f4cd3a1';
+
+// helper function that can be used to fetch data from an API endpoint  
+  getResource = async (url) => { // asynchronous arrow function that takes in a url parameter
+    // 'await' is used to wait for response to come back before moving on to next line of code
+      // 'fetch' send GET request to specified URL
+    let res = await fetch(url);  // response object is assigned to 'res' variable
+      // this 'if' statement checks whether response status code is not in 200-299 range
+      if (!res.ok) { throw new Error(`Could not fetch ${url}, status: ${res.status}`); } 
+        // 'await' is used to wait for JSON parsing to complete before returning data
+        return await res.json();
+  }
+/* 
+   In summary, 'getResource' function fetches data from specified API endpoint, 
+   checks response status code for errors, and returns parsed JSON data if request is successful.
+*/
+
+
+  getAllCharacters = () => {
+    // code from block 'INTERACTIVE API TESTER'
+    return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  }
+
+  getCharacter = (id) => {
+    // code from block 'INTERACTIVE API TESTER'
+    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  }
+}
+
+export default MarvelService;
